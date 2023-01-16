b8d73d9b86ceb9804f9b913c1988fe20f9fc6696
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-01-16 11:53:31 -0700

    add create a user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,13 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    require 'pry'; binding.pry
-    key = SecureRandom.hex(64)
-    require 'pry'; binding.pry
-    User.new(user_params)
-  end
+    # require 'pry'; binding.pry
 
-  private 
-
-  def user_params
-    params.require(:user).require(:data).permit(:name, :email, :api_key)
+    user = User.new(name: params[:name], email: params[:email], api_key: User.generate_api_key)
+    # require 'pry'; binding.pry
+    if user.save
+      render json: UserSerializer.new(user), status: 201
+    else
+      render json: { error: 'User not created'}, status: 404
+    end
   end
 end
